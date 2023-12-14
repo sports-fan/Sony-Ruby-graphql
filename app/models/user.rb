e489ac99dfaf77052e021cbad7b973c00e11dbc7
sport-fan <lovesport801@gmail.com>
2023-12-13 17:47:43 -0800

    Implement signin mutation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validate :name, presence: true
-  validate :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
