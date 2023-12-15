ae43c4a5d8cd79d319741beae800a2b99eed3b3a
sport-fan <lovesport801@gmail.com>
2023-12-15 10:27:58 -0800

    Implement vote

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_14_020118) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_15_173835) do
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
@@ -28,5 +28,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_14_020118) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "link_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "links", "users"
+  add_foreign_key "votes", "links"
+  add_foreign_key "votes", "users"
 end
